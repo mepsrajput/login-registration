ec8c63b1a2ebdbbc96fc35c5a204b46910338b44
Pradeep Singh <viveksingh12126@gmail.com>
2018-02-25 22:12:50 +0530

    Add files via upload

--- register.php
+++ register.php
@@ -81,19 +81,6 @@
 	include("header.html");
 
 ?>
- 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
-<body>
     <div class="wrapper">
         <h2>Sign Up</h2>
         <p>Please fill this form to create an account.</p>
@@ -125,8 +112,6 @@
             <p>Already have an account? <a href="login.php">Login here</a>.</p>
         </form>
     </div>    
-</body>
-</html>
 <?php
 
 	include("footer.html");
