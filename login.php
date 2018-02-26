61f239f9abbf5ea6656fc7dcd29de2109604efc3
Pradeep Singh <viveksingh12126@gmail.com>
2018-02-26 14:13:09 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -1,35 +1,66 @@
 <?php
-// Include config file
-require_once 'dbconf.php';
- 
-// Define variables and initialize with empty values
-$email = $password = "";
-$email_err = $password_err = "";
- 
-include("header.html");
 
-?>
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
-                <label>Email</label>
-                <input type="text" name="email"class="form-control" value="<?php echo $email; ?>">
-                <span class="help-block"><?php echo $email_err; ?></span>
-            </div>    
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control">
-                <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
-            </div>
-            <p>Dont have an account? <a href="register.php">Sign up now</a>.</p>
-        </form>
-    </div> 
+	require_once 'dbconf.php';
 
-<?php
-	include("footer.html");
+	if($user->is_loggedin()!="")
+	{
+	 $user->redirect('index.php');
+	}
+
+	if(isset($_POST['btn-login']))
+	{
+	 $email = $_POST['email'];
+	 $password = $_POST['password'];
+	  
+	 if($user->login($email,$password))
+	 {
+	  $user->redirect('index.php');
+	 }
+	 else
+	 {
+	  $error = "Wrong Details !";
+	 } 
+	}
+	include("header.html");
 ?>
+	
+	<link rel="stylesheet" href="userForm.css">
+
+	<div class="container-fluid">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-sm-12 text-center">
+					<div class ="userForm">
+						
+						<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">							<p style="font-size: 16px">Login with your Social Media Account</p>
+							
+							<button class="loginBtn loginBtn--facebook">Login with Facebook</button>
+							<button class="loginBtn loginBtn--google">Login with Google</button><br><br>
+							
+							<div><b>OR</b></div><br>
+							
+							<p style="font-size: 16px">Please fill in your credentials to login.</p>
+									
+							<div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+								<input type="text" name="email" placeholder="Email ID" class="form-control" value="<?php echo $email; ?>">
+								<span class="help-block"><?php echo $email_err; ?></span>
+							</div>    
+							<div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+								<input type="password" name="password" placeholder="Password" class="form-control">
+								<span class="help-block"><?php echo $password_err; ?></span>
+							</div>
+							<div class="form-group">
+								<input type="submit" class="btn btn-primary" value="Login" name="btn-login">
+								<input type="reset" class="btn btn-default" value="Reset">
+							</div>
+									
+							<p>Dont have an account? <a href="register.php">Sign up now</a>.</p>
+								
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php include("footer.html"); ?>
